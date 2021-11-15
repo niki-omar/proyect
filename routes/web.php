aee4f1d626c1d85cbf485c14f21527fb21466e62
nikiextremo <lokoxknosexk@hotmail.com>
2021-11-15 11:35:31 -0500

    cambio

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,19 @@
 <?php
-
 use App\Http\Controllers\EntryController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\GuestController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+use App\Http\Controllers\UserController;
 Route::get('/','GuestController@index');
-
 Auth::routes();
 // Route::get('/home','HomeController@index')->name('home'); Laravel 7
-
 Route::get('/home', [HomeController::class, 'index']);
-
 // Route::get('/entries/create', 'EntryController@create'); Laravel 7
-Route::get('/entries/create', [EntryController::class, 'create']);
+Route::get('/entries/create',[EntryController::class, 'create']);
 // Route::post('/entries', 'EntryController@store'); Laravel 7
-Route::post('entries/create', [EntryController::class, 'store']);
-
-Route::get('/entries/{entry}',  [GuestController::class, 'show']);
-
-Route::get('/entries/{entry}/edit',  [EntryController::class, 'edit']);
-Route::put('/entries/{entry}', [EntryController::class, 'update']);
-
-Route::get('/users/{user}',  [UserController::class, 'show']);
+Route::post('entries/create',[EntryController::class, 'store']);
+Route::get('/entries/{entry}',[GuestController::class, 'show']);
+Route::get('/entries/{entry}/edit',[EntryController::class, 'edit']);
+Route::put('/entries/{entry}',[EntryController::class, 'update']);
+Route::get('/users/{user}',[UserController::class, 'show']);
